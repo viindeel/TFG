eb029ec970e0f17faa2d5699f581c9577bb46bd5
Jorge Vindel <jorge2020vindel@gmail.com>
2025-03-28 08:57:40 +0100

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { LeccionesComponent } from './components/lecciones/lecciones.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 import { BrainstormingLessonComponent } from './components/lessons/brainstorming-lesson/brainstorming-lesson.component'; // Importa el nuevo componente
+import { EquilibrioVidaLaboralComponent } from './components/lessons/equilibrio-vida-laboral/equilibrio-vida-laboral.component';
+import { MonopoliosLessonComponent } from './components/lessons/monopolios-lesson/monopolios-lesson.component';
 
 
 export const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'lecciones', component: LeccionesComponent },
   { path: 'lecciones/brainstorming', component: BrainstormingLessonComponent }, // Nueva ruta
+  { path: 'lecciones/monopolios', component: MonopoliosLessonComponent }, // Nueva ruta
+  { path: 'lecciones/equilibrio-vida-laboral', component: EquilibrioVidaLaboralComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // Esta línea asegura que la ruta '' redirija a '/inicio'
   { path: '**', component: PaginaNoEncontradaComponent } // Ruta para cualquier otra URL
