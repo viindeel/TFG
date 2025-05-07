ec7876bc6bcfe1bf3a5f34461b5c510d15242f1f
Jorge Vindel <jorge2020vindel@gmail.com>
2025-05-07 11:02:35 +0200

    muchos cambios nice sobre todo visuales

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -2,23 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule,
+    RouterLink,
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private snackBar: MatSnackBar) { }
+  showVideoModal: boolean = false;
+
+  // --- Propiedad para la URL del video saneada ---
+  sanitizedVideoUrl: SafeResourceUrl | undefined;
+
+  private videoPath: string = 'assets/videos/video_tfg.mp4';
+
+  constructor(private snackBar: MatSnackBar, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    // --- Inicializar la URL del video ---
+    // Aquí saneamos la URL del video para evitar problemas de seguridad
+    // Esto es necesario para que Angular permita cargar videos locales
+    this.sanitizedVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoPath);
+
     this.snackBar.open('¡Bienvenido a Palabras al poder! ¿Cúanto serás capaz de aprender? ', 'Cerrar', {
-      duration: 3000,
+      duration: 5000,
       verticalPosition: 'top',
-      });
+      horizontalPosition: 'center',
+      panelClass: ['welcome-snackbar']
+    });
+  }
+
+  openVideo(): void {
+    this.showVideoModal = true;
+  }
+
+  closeVideo(): void {
+    this.showVideoModal = false;
   }
 }
